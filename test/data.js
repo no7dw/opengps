f6a381f18df71d2cc816b2ebe348009a56d64ef3
no7dw <no7david@gmail.com>
2016-12-16 01:52:20 +0800

    add test

--- test/data.js
+++ test/data.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const gps = require('../index')
 
 const reqAccu = "https://www.opengps.cn/Data/IP/IPLocHiAcc.ashx"
@@ -44,15 +45,25 @@ const failResult = {
 
 describe('request ip address', () => {
     it('#1 normal ip request should OK', function * () {
-        ip = '116.22.135.246'
-        const ipResult = yield gps.getAddress(ip)
+        const ip = '116.22.135.246'
+        const ipResult = yield gps.getAddress2(ip)
         console.log(ipResult)
         ipResult.values[0].should.be.equal(161)
     })
     it('#2 no detail ip request should not have detail', function * () {
-        ip = '8.8.8.8'
+        const ip = '8.8.8.8'
         const ipResult = yield gps.getAddress(ip)
         console.log(ipResult)
         ipResult.values[0].should.be.equal(167)
     })
 })
+
+// co(function*() {
+//     const re = yield this.getAddress('116.22.135.245');
+//     console.log('result:',re)    
+// }).then(function (value) {
+//     console.log(value);
+// }, function (err) {
+//     console.error(err.stack);
+// });
+
