14dd0520c71af2262b5a221a715bc3482e8680e9
no7dw <no7david@gmail.com>
2016-12-16 16:04:46 +0800

    test not pass

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ### opengps: ip to gps address 
+
+example: 
+
+    `curl --data "ip=116.22.135.246" https://www.opengps.cn/Data/IP/IPLocHiAcc.ashx`
+
--- test/data.js
+++ test/data.js
@@ -1,23 +1,25 @@
 'use strict'
 
 const gps = require('../index')
-
+const should = require('should');
 const reqAccu = "https://www.opengps.cn/Data/IP/IPLocHiAcc.ashx"
 const reqNorm = "https://www.opengps.cn/Data/IP/IPSearch.aspx"
 
 describe('request ip address', () => {
-    it('#1 normal ip request should OK', function * () {
+    it('#1 normal ip request should OK', function * (done) {
         const ip = '116.22.135.246'
         console.log('wwwip', ip)
-        // const ipResult = yield gps.getAddress(ip)
-        // console.log(ipResult)
-        // ipResult.values[0].should.be.equal(161)
+        const ipResult = yield gps.getAddress(ip)
+        console.log(ipResult)
+        ipResult.values[0].should.be.equal(161)
+        done()
     })
-    it('#2 no detail ip request should not have detail', function * () {
+    it('#2 no detail ip request should not have detail', function * (done) {
         const ip = '8.8.8.8'
-        // const ipResult = yield gps.getAddress(ip)
-        // console.log(ipResult)
-        // ipResult.values[0].should.be.equal(167)
+        const ipResult = yield gps.getAddress(ip)
+        console.log(ipResult)
+        ipResult.values[0].should.be.equal(167)
+        done()
     })
 })
 
